578f35f395b80e10d3bee0cdccd5f4d4f5f058c8
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2024-03-24 21:53:41 -0300

    the final of v1.0.2 => text error fixing

--- components/Report.tsx
+++ components/Report.tsx
@@ -4,11 +4,11 @@ import React from "react";
 function Report() {
   return (
     <div className="flex w-10/12  m-auto min-h-screen font-syne">
-      <div className="p-4 col-span-6 w-full">
+      <div className=" pb-28 p-4 col-span-6 w-full">
         <p className="text-justify italic font-jura">{me.value}</p>
       </div>
       <div className=" boder-l border-r "></div>
-      <div className=" pb-20 col-span-6 w-full p-4 text-justify italic font-jura flex items-end">
+      <div className=" pb-28 col-span-6 w-full p-4 text-justify italic font-jura flex items-end">
         <p>{me.ambs}</p>
       </div>
     </div>
